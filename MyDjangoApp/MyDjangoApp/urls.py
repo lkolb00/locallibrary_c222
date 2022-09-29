3f120c260f340b90c860701e7d6462f1b6cc985a
Lucas Kolb <lkolb@unomaha.edu>
2022-09-29 01:32:01 -0500

    1.0
    1.0

--- MyDjangoApp/MyDjangoApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('', include('mailingsystem.urls')),
-    path(r'^admin/', admin.site.urls),
-    path(r'^mailingsystem/', include('mailingsystem.urls'))
-]
